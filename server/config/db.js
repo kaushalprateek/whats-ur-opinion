4959f7f796f71e4e0dbdd08f0a1dfc6e62f33c13
kaushalprateek <pkausha4@my.centennialcollege.ca>
2019-04-20 03:41:30 -0400

    db  mlab update

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  URI: "mongodb://localhost/test",
-
+  // URI: "mongodb://localhost/test",
+  URI: "mongodb://kaushal:a123456@ds018258.mlab.com:18258/assignment02",
   secret: "SomeSecret"
 };
